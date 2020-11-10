ee09acb6f7169597100c0a9d27434e1106b02167
sam6030327 <72539881+sam6030327@users.noreply.github.com>
2020-11-10 14:52:58 +0800

    Android-Studio-12-ImageBtn-main

---
+++ Android-Studio-12-ImageBtn-main/android_num_12_ImageBtn/app/src/main/java/com/example/android_num_12_imagebtn/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android_num_12_imagebtn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ImageView imageView = (ImageView)findViewById(R.id.imageButton);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageResource(R.drawable.maiin);
+            }
+        });
+    }
+}
